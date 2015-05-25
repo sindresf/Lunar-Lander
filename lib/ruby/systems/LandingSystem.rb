56b69e432f14ad8f993d30f49481411dde45fcd0
mr.Fjermestad <sindresfjermestad@gmail.com>
2015-05-25 14:31:11 +1000

    fixed the landing for player 2, the false return was placed based on single player.

--- lib/ruby/systems/LandingSystem.rb
+++ lib/ruby/systems/LandingSystem.rb
@@ -4,31 +4,30 @@ class LandingSystem < System
   PIXEL_FUDGE = 2
   MAX_SPEED = 5
   def process_one_game_tick(delta, entity_mgr)
-    landable_entities = entity_mgr.get_all_entities_with_component_of_type(Landable)
-    pad_entities = entity_mgr.get_all_entities_with_component_of_type(Pad)
+    landable_entities = entity_mgr.get_all_entities_with_component_of_type Landable
+    pad_entities = entity_mgr.get_all_entities_with_component_of_type Pad
     landable_entities.each do |entity|
       location_component = entity_mgr.get_component_of_type(entity, SpatialState)
       renderable_component = entity_mgr.get_component_of_type(entity, Renderable)
       bl_x = location_component.x
       bl_y = location_component.y
-      bc_x = bl_x + (renderable_component.width/2)
+      bc_x = bl_x + (renderable_component.width / 2)
       br_x = bl_x + renderable_component.width
       br_y = bl_y
       pad_entities.each do |pad|
         pad_loc_component = entity_mgr.get_component_of_type(pad, SpatialState)
         pad_rend_component = entity_mgr.get_component_of_type(pad, Renderable)
         ul_x = pad_loc_component.x
-        ul_y = pad_loc_component.y+pad_rend_component.height
-        ur_x = ul_x+pad_rend_component.width
+        ul_y = pad_loc_component.y + pad_rend_component.height
+        ur_x = ul_x + pad_rend_component.width
         ur_y = ul_y
-        if (bl_y>=ul_y-PIXEL_FUDGE && bl_y <= ul_y+PIXEL_FUDGE) &&
-        ( bc_x>=ul_x && bc_x <= ur_x) &&
-        ( location_component.dy <= MAX_SPEED)
+        if (bl_y>=ul_y - PIXEL_FUDGE && bl_y <= ul_y + PIXEL_FUDGE) &&
+        (bc_x>=ul_x && bc_x <= ur_x) &&
+        (location_component.dy <= MAX_SPEED)
           return true
         end
       end
-
-      return false
     end
+    return false
   end
 end
